39ef6b16f30c4d0c28650ad60fa9361a16188610
majiashu <798797750@qq.com>
2020-09-21 16:48:39 +0800

    更新

--- src/main/java/cn/majs/array/FindErrorNumSolution.java
+++ src/main/java/cn/majs/array/FindErrorNumSolution.java
@@ -51,5 +51,10 @@ public class FindErrorNumSolution {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 11, 12};
         System.out.println(findErrorNum(nums));
+
+        // 打印出数组
+        for(int i=0; i<findErrorNum(nums).length; i++){
+            System.out.print(findErrorNum(nums)[i]+"\t");
+        }
     }
 }
